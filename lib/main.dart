813e327115308ac5a31004394ac4c960e9cb066a
jaromi.istvan <jaromi.istvan@gmail.com>
2023-12-24 17:13:30 +0100

    button pressed

--- lib/main.dart
+++ lib/main.dart
@@ -37,14 +37,18 @@ class DicePage extends StatelessWidget {
       child: Row(
         children: [
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
+            child: TextButton(
+              onPressed: () {
+                print('left button got pressed');
+              },
               child: Image.asset('images/dice1.png'),
             ),
           ),
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
+            child: TextButton(
+              onPressed: () {
+                print('right button got pressed');
+              },
               child: Image.asset('images/dice1.png'),
             ),
           ),
